0bb83b1c16b2eb82aa22cc0917159c9d69b07fce
Justin Kwan <jkwan19@gmail.com>
2021-05-24 15:15:51 -0700

    Display thumbnails

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -3,6 +3,7 @@ import React from 'react';
 const VideoItem = ({ video }) => {
   return (
     <div>
+      <img src={video.snippet.thumbnails.medium.url} />
       {video.snippet.title}
     </div>
   )
