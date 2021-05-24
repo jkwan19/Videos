83c282ee3cc51414cc28c779fba0184bcceaab75
Justin Kwan <jkwan19@gmail.com>
2021-05-24 15:41:17 -0700

    Fix video app container style and view

--- src/components/App.js
+++ src/components/App.js
@@ -31,13 +31,21 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="ui-continer">
+      <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit}/>
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList
-          videos={this.state.videos}
-          onVideoSelected={this.onVideoSelected}
-          />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelected={this.onVideoSelected}
+                />
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
